1d29c61870a172c67868fa50a3ae7b8964684904
Abimbola Awopetu <126657330+Tomini1@users.noreply.github.com>
2025-08-04 22:20:51 +0100

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     title = forms.CharField(max_length=255)
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    author = forms.CharField(max_length=255)
+    publication_year = forms.IntegerField()

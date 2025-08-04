84a19632e23a857afab77adf4e913ad9d056bc5b
Abimbola Awopetu <126657330+Tomini1@users.noreply.github.com>
2025-08-04 21:58:15 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from .forms import BookSearchForm
-
+from django.contrib.auth.decorators import permission_required
 
 def secure_form_view(request):
     form = BookSearchForm(request.POST or None)
@@ -13,3 +13,15 @@ def secure_form_view(request):
         books = Book.objects.filter(title__icontains=title)
         return render(request, 'bookshelf/book_list.html', {'books': books})
     return render(request, 'bookshelf/form_example.html', {'form': form})
+
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # Your view logic here
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    # Your view logic here
+    pass

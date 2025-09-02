e5f993e1f4e82ca27c22e13fa39e8629ea78425b
Abimbola Awopetu <126657330+Tomini1@users.noreply.github.com>
2025-09-02 17:22:21 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -29,3 +29,12 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
+
+
+urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+]
